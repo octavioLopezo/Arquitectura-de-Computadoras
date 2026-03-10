fd1c7c05f9033c27dab54b82e78f6044a5ba853d
octavioLopezo <octavio.e.lopezo@gmail.com>
2026-03-10 15:47:38 -0600

    Delete FA directory

--- FA/OR32Comp.v
+++
@@ -1,9 +0,0 @@
-module OR32Comp (
-	input [31:0]Oo1,
-	input [31:0]Oo2,
-	output [31:0]RO
-	);
-	
-assign RO = Oo1+Oo2;
-
-endmodule
--- FA/SLT32.v
+++
@@ -1,9 +0,0 @@
-module SLT32 (
-	input [31:0]SLTo1,
-	input [31:0]SLTo2,
-	output [31:0]RSLT
-	);
-	
-assign RSLT = SLTo1 < SLTo2 ? 1:0;
-
-endmodule
